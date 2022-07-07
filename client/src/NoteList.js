065b344fc1c55c0f1279ff97b4efe9faad50b196
Yuxin Ye <yuxin_ye@protonmail.com>
2022-07-07 18:28:17 +1000

    set up styling for the project

--- client/src/NoteList.js
+++ client/src/NoteList.js
@@ -1,5 +1,22 @@
 import { Stack } from "@chakra-ui/react";
+import { UiNote } from "./shared-ui";
 
 export function NoteList() {
-  return <Stack spacing={4}></Stack>;
+  const notes = [
+    { content: "Note 1", category: { label: "Work" } },
+    { content: "Note 2", category: { label: "Work" } },
+  ];
+  return (
+    <Stack spacing={4}>
+      {notes?.map((note, index) => (
+        <UiNote
+          key={index}
+          content={note.content}
+          category={note.category.label}
+        >
+          {note.content}
+        </UiNote>
+      ))}
+    </Stack>
+  );
 }
