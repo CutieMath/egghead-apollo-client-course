793f5b6da01b193ccc6781cd283f7822bd4d6c8c
Yuxin Ye <yuxin_ye@protonmail.com>
2022-07-07 18:02:53 +1000

    set up the initial state of the project (client)

--- client/src/NoteList.js
+++ client/src/NoteList.js
@@ -1,31 +1,5 @@
 import { Stack } from "@chakra-ui/react";
-import { UiNote } from "./shared-ui";
-import { gql, useQuery } from "@apollo/client";
-
-const ALL_NOTES_QUERY = gql`
-  query GetAllNotes {
-    notes {
-      id
-      content
-      category {
-        label
-      }
-    }
-  }
-`;
 
 export function NoteList() {
-  const { data } = useQuery(ALL_NOTES_QUERY);
-  const notes = data?.notes;
-  return (
-    <Stack spacing={4}>
-      {notes?.map((note) => (
-        <UiNote
-          key={note.id}
-          content={note.content}
-          category={note.category.label}
-        ></UiNote>
-      ))}
-    </Stack>
-  );
+  return <Stack spacing={4}></Stack>;
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,21 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
-  cache: new InMemoryCache(),
-});
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
-      <ApolloProvider client={client}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ApolloProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
