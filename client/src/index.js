f1db7720bd5932597c3ab371ca80657b528fbd72
Yuxin Ye <yuxin_ye@protonmail.com>
2022-07-14 18:25:58 +1000

    learnt retry

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,31 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from,
+} from "@apollo/client";
+import { RetryLink } from "@apollo/client/link/retry";
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000/graphql",
+});
+
+const retryLink = new RetryLink({
+  delay: {
+    initial: 2000,
+    max: 2000, // max wait time
+    jitter: false, // randomize the time between intervals => better to set true
+  },
+});
 
 // point to the server
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache(), // used local cache
+  link: from([retryLink, httpLink]),
 });
 
 ReactDOM.render(
