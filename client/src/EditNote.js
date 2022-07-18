f6c9dd37c0bc66a7bf06de0f30d6f74657d81ee8
Yuxin Ye <yuxin_ye@protonmail.com>
2022-07-18 17:54:28 +1000

    construct gql query

--- client/src/EditNote.js
+++ client/src/EditNote.js
@@ -1,4 +1,14 @@
+import { gql, useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
+
+const GET_NOT = gql`
+  query GetNote($id: String!) {
+    note(id: $id) {
+      id
+      content
+    }
+  }
+`;
 
 export function EditNote() {
   let { noteId } = useParams();
