ac0d49200f499460e81aea391f8be1b5aa038b5a
Yuxin Ye <yuxin_ye@protonmail.com>
2022-07-20 17:54:18 +1000

    add method to update category name

--- client/src/EditCategories.js
+++ client/src/EditCategories.js
@@ -12,8 +12,10 @@ export const ALL_CATEGORIES_QUERY = gql`
 `;
 
 export function EditCategories() {
+  // First get all category data
   const { data } = useQuery(ALL_CATEGORIES_QUERY);
 
+  // Then post to update
   const [updateCategory] = useMutation(gql`
     mutation UpdateCategory($categoryId: String!, $categoryLabel: String!) {
       updateCategory(id: $categoryId, label: $categoryLabel) {
